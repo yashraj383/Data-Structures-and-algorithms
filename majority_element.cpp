34b0999c3e531687849e2526caa3c53593545528
Aditya Chaudhary <aditya.ch.304@gmail.com>
2020-10-03 13:01:48 +0530

    A Divide And Conquer problem for finding a majority element in an array.

--- majority_element.cpp
+++ majority_element.cpp
@@ -1,45 +1,40 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
-using namespace std;
-long int get_majority_element(vector<long int> a,int left, int right) {
- // if (left == right) return -1;
-  //if (left + 1 == right) return a[left];
-  //else
-  /*{
-      int mid = (left + right) / 2;
-      left = mid + 1;
-      get_majority_element(a, left, mid - 1);
-      right =  mid - 1;
-      get_majority_element(a, mid + 1, right);
 
-  }*/
- long int n = a.size();
- long int temp = a[0], tag = 1, i;
+using std::vector;
+
+int get_majority_element(vector<int> &a, int left, int right) {
+  if (left == right) return -1;
+  if (left + 1 == right) return a[left];
+  //write your code here
   sort(a.begin(), a.end());
-  for(i = 0; i < a.size() - 1; i++){
-    if(a[i + 1] == a[i] && tag <= n /2){
-        temp = a[i];
-        tag++;
-    }
-    else if(tag > n/2){
-        return temp;
-    }
+  int count;
 
-   else if(a[i + 1] != a[i] && tag <= n /2){
-        temp = a[i];
-        tag = 1;
+  for(int i = 0; i<a.size()-1; i++)
+  {
+    int count =1;
+    while(a[i]==a[i+1])
+    {
+      count++;
+      i++;
+    }
+    //std::cout << count <<"-ct-"<<a[i] <<'\n';
+    if(count > (a.size())/2 ){
+      std::cout << 1 <<'\n' ;
+      return 0;
     }
   }
-
+  std::cout << 0<<'\n';
+  return 0 ;
 }
 
 int main() {
   int n;
-  cin >> n;
-  vector<long int> a(n);
-  for (int i = 0; i < a.size(); ++i) {
-    cin >> a[i];
+  std::cin >> n;
+  vector<int> a(n);
+  for (size_t i = 0; i < a.size(); ++i) {
+    std::cin >> a[i];
   }
- cout << get_majority_element(a, 0, a.size()) << '\n';
+  get_majority_element(a, 0, a.size()) ;
 }
